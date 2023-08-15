97db5ada106df818dd77f0cb0809638b15531a23
ReallySadWolf <petargabrijel.kedmenec@gmail.com>
2023-08-15 15:25:58 +0200

    moving worlds into a seperate page

---
+++ src/components/pages/WorldsPage.vue
@@ -0,0 +1,54 @@
+<template>
+    <WorldsGrid ref="WorldsGrid" :key="componentKey"></WorldsGrid>
+    <WorldModal ref="WorldModal"></WorldModal>
+    <CreateWorldButton></CreateWorldButton>
+</template>
+  
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import WorldModal from './components/worlds/WorldModal.vue';
+import WorldsGrid from './components/worlds/WorldsGrid.vue';
+import CreateWorldButton from './components/worlds/CreateWorldButton.vue';
+
+export default {
+    name: 'WorldsPage',
+    components: {
+        WorldsGrid,
+        WorldModal,
+        CreateWorldButton
+    },
+    methods: {
+        displayCreateWorldModal() {
+            this.$refs.WorldModal.toggleModal();
+        },
+        displayEditWorldModal(worldName) {
+            console.log(worldName);
+            this.$refs.WorldModal.toggleEditModal(worldName);
+        },
+        deleteWorld(worldName) {
+            var elementToDelete = document.getElementById(worldName);
+            elementToDelete.remove();
+        },
+        reloadGrid() {
+            this.componentKey += 1;
+        }
+    },
+    data() {
+        return {
+            componentKey: 0
+        }
+    }
+};
+</script>
+  
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
+  
